49280188838e2ea189f134bcba3c764096585bac
kokoalita <kokoalita@outlook.es>
2024-02-15 16:39:11 +0100

    commit message to test

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
 
 /*function App() {
@@ -32,7 +34,7 @@ import './App.css'
   )
 }
 */
-const App = () => {
+/*const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
@@ -46,5 +48,34 @@ const App = () => {
       </p>
     </div>
   )
+}*/
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello world {props.name}, you are {props.age} years old</p>
+    </div>
+  )
 }
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Eve' age={26 + 7} />
+      <p>{friends}</p>
+      <Footer />
+    </>
+  )
+}
+
 export default App
