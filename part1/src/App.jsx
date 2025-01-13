9900880d8769282b7803969f6bcbcee2ec691752
kokoalita <kokoalita@outlook.es>
2025-01-13 20:09:00 +0100

    parte 2-6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+//import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  /*
+  useEffect(() => {
+  console.log('effect')
+
+  const eventHandler = response => {
+    console.log('promise fulfilled')
+    setNotes(response.data)
+  }
+
+  const promise = axios.get('http://localhost:3001/notes')
+  promise.then(eventHandler)
+}, [])
+  */
+  const hook = () => {
+    console.log('effect')
+    noteService.getAll()
+      .then(initialNotes => {
+        console.log('promise fulfilled')
+        const nonExisting = {
+          id: 10000,
+          content: 'This note is not saved to server',
+          important: true,
+        }
+        setNotes(initialNotes.concat(nonExisting))
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -19,16 +52,42 @@ const App = (props) => {
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-    
-    console.log('button clicked', event.target)
-    console.log(event.target)
+    //axios
+      //.post('http://localhost:3001/notes', noteObject)
+    noteService.create(noteObject)
+      .then(returnedNote => {
+        console.log(returnedNote)
+
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+        console.log('button clicked', event.target)
+        console.log(event.target)
+      })   
   }
 
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
+
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    //axios.put(url, changedNote)
+    noteService.update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(n => n.id === id ? returnedNote : n))
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -38,7 +97,13 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note =>  <Note key={note.id} note={note} />)}
+        {notesToShow.map(note =>  <Note 
+                                      key={note.id} 
+                                      note={note} 
+                                      toggleImportance={() => toggleImportanceOf(note.id)}
+                                  />
+                        )
+        }
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
@@ -46,15 +111,6 @@ const App = (props) => {
       </form>   
     </div>
   )
-}
-const addNote = (event) => {
-  event.preventDefault()
-  const newNote = {
-    id: notes.length + 1,
-    content: event.target.value,
-  }
-  setNotes([...notes, newNote])
-  event.target.value = ''
 }
 
 export default App
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,26 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+/*
+import axios from 'axios'
+
+axios.get('http://localhost:3001/notes').
+then(response => {
+  const notes = response.data
+  console.log('hola ', notes, 'all ', response)
+
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App notes={notes} />
+    </StrictMode>,
+  )
+})
+*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App notes={notes} />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>)
+
