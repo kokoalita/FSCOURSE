f083013a331bfbc0e2d77aac07990997a851880d
efuster@s2i.es <efuster>
2025-01-16 10:52:28 +0100

    Part 2 total

---
+++ part1/src/part2/components/Notification.jsx
@@ -0,0 +1,17 @@
+
+import '.././content/index.css'
+
+const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
+
+  
+export default Notification
