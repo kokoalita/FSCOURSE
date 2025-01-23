6fda7085555e1c868f067988509e021ffdfebd8a
kokoalita <kokoalita@outlook.es>
2025-01-23 21:35:45 +0100

    adding static web -vite-  to server

--- part1/src/services/notes.js
+++ part1/src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3002/api/notes'
+//const baseUrl = 'http://localhost:3002/api/notes'
+const baseUrl = 'https://fscourse-serveer.onrender.com/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
