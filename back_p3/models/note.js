1953988701d1cec22274334515d4bc06a1b54134
kokoalita <kokoalita@outlook.es>
2025-01-31 19:20:21 +0100

    .

--- back_p3/models/note.js
+++ back_p3/models/note.js
@@ -17,14 +17,14 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const noteSchema = new mongoose.Schema({
-    content:  {
-      type: String,
-      minLength: 5,
-      required: [true, 'content is required']
-    },
-    important: Boolean,
-  })
+const noteSchema = new mongoose.Schema({
+  content:  {
+    type: String,
+    minLength: 5,
+    required: [true, 'content is required']
+  },
+  important: Boolean,
+})
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
