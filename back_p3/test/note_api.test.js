7f60241b005a45c863325fe666c700492edc039a
kokoalita <kokoalita@outlook.es>
2025-02-10 21:54:36 +0100

    202502102154 part4-3

--- back_p3/test/note_api.test.js
+++ back_p3/test/note_api.test.js
@@ -17,7 +17,7 @@ const initialNotes = [
       important: true,
     },
     {
-      content: 'Browser can execute only JavaScript',
+      content: 'Browser can execute only JavaScript  55',
       important: true,
     },
   ]
@@ -52,6 +52,42 @@ test('the first note is about HTTP methods', async () => {
 
     const contents = response.body.map(e => e.content)
     assert(contents.includes('HTML is easy'))
+})
+
+test('a valid note can be added ', async () => {
+    const newNote = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+    }
+
+    await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map(r => r.content)
+
+    assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+    assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test('note without content is not added', async () => {
+    const newNote = {
+      important: true
+    }
+  
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(400)
+  
+    const response = await api.get('/api/notes')
+  
+    assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 after(async () => {
