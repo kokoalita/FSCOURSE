a79fd8e41fee3157868a2a1f50093fc8cd5d370f
kokoalita <kokoalita@outlook.es>
2025-02-12 19:28:04 +0100

    22502121928

--- back_p3/utils/middleware.js
+++ back_p3/utils/middleware.js
@@ -14,11 +14,15 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-
+  console.log('que paso ' , error.name , '--')
   if (error.name === 'CastError') {k
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  }
+  else{
+    return response.status(401).json({ error: error.message })
+
   }
 
   next(error)
