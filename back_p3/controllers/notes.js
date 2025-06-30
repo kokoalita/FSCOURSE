9a073de8894b3866b43e25e0de4ca2a4626a510c
kokoalita <kokoalita@outlook.es>
2025-06-30 21:50:41 +0200

    return to this

--- back_p3/controllers/notes.js
+++ back_p3/controllers/notes.js
@@ -1,11 +1,14 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+
 
 notesRouter.get('/', async (request, response) => {
   /*Note.find({}).then(notes => {
     response.json(notes)
   })*/
   const notes = await Note.find({})
+  .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -21,6 +24,7 @@ notesRouter.get('/:id', async (request, response, next) => {
     .catch(error => next(error))*/
    // try {
       const note = await Note.findById(request.params.id)
+      .find({}).populate('user', { username: 1, name: 1 })
       if (note) {
         response.json(note)
       } else {
@@ -34,13 +38,17 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 //try{
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 /*} catch(exception){
   next(exception)
---
+++ part_7/Part7/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './reducer'
+
+const store = configureStore({
+  reducer: reducer,
+});
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'INCREMENT'
+    })
+  }
+  console.log(store.getState())
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+console.log(store.getState())
+store.dispatch({ type: 'ZERO' })
+store.dispatch({ type: 'DECREMENT' })
+console.log(store.getState())
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button>ok</button> 
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
