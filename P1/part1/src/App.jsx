01369ae71b8a5cd97cc3978c6ab707d3447bb317
kokoalita <kokoalita@outlook.es>
2026-07-01 19:53:05 +0200

    part 1 finalized!

--- P1/part1/src/App.jsx
+++ P1/part1/src/App.jsx
@@ -248,7 +248,7 @@ const App = () => {
 */
 
 //Part 1 - c -refactoring the components
-
+/*
 const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({ onClick, text }) =>  <button onClick={onClick}> {text} </button> 
@@ -278,6 +278,65 @@ const App = () => {
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero" />
       <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+}*/
+
+// Par 1 .d conditiona rendering 
+
+
+const Button = ({ onClick, text }) =>  <button onClick={onClick}> {text} </button> 
+
+const History = (props) => {
+  console.log('props',props)
+  // debugger -- do not forget to use this
+  console.log('continue', props)
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRigth = right + 1
+    setRight(updatedRigth)
+
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text= {left} />
+      <Button onClick={handleRightClick} text={right} />
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+      
+      <History allClicks={allClicks} />
     </div>
   )
 }
--- P1/part1/src/main.jsx
+++ P1/part1/src/main.jsx
@@ -3,16 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-/*
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-  */App
   
-createRoot(document.getElementById('root')).render(
-    <App />,
-)
 
 
