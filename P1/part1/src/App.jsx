f5b7d2f2880934f1bc69ef3fb3bf294f859e0ae7
kokoalita <kokoalita@outlook.es>
2026-07-02 22:42:01 +0200

    part 2 - b - Saving the notes in the component state

--- P1/part1/src/App.jsx
+++ P1/part1/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
+import Note from './components/Note'
+
 
 const App = (props) => {
-  const { notes } = props
-
+  const [notes, setNotes] = useState(props.notes)
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
+      
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>  
     </div>
+    
   )
 }
 
