cadeda198f9054d536461a31816852ae1f7d3b06
kokoalita <kokoalita@outlook.es>
2026-07-05 20:22:11 +0200

    part 3 - a- receiving data

--- P3/partA/index.js
+++ P3/partA/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -24,7 +26,34 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
+    console.log(request.headers)
   response.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body || !body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -45,6 +74,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
   
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
