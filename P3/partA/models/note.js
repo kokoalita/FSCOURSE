a9164e3ae508396b6f2b6a5684df78e214841de8
kokoalita <kokoalita@outlook.es>
2026-07-17 18:03:10 +0200

    p4b init db

--- P3/partA/models/note.js
+++ P3/partA/models/note.js
@@ -1,23 +1,4 @@
-require('dotenv').config()
-
 const mongoose = require('mongoose')
-const dns = require('dns')
-
-dns.setServers(['8.8.8.8', '1.1.1.1'])
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
